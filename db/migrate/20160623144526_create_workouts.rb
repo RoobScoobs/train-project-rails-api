4a43c7653d93d7455aba25c5a0b313019047ae1b
RoobScoobs <iruben229@gmail.com>
2016-06-23 14:31:49 -0400

    Change workouts migration
    Add NOT NULL to column names

--- db/migrate/20160623144526_create_workouts.rb
+++ db/migrate/20160623144526_create_workouts.rb
@@ -1,12 +1,12 @@
 class CreateWorkouts < ActiveRecord::Migration
   def change
     create_table :workouts do |t|
-      t.references :user, index: true, foreign_key: true
-      t.string :name_of_workout
-      t.text :description
-      t.decimal :total_distance
-      t.text :completion_time
-      t.date :completion_date
+      t.references :user, index: true, foreign_key: true, null: false
+      t.string :name_of_workout, null: false
+      t.text :description, null: false
+      t.decimal :total_distance, null: false
+      t.text :completion_time, null: false
+      t.date :completion_date, null: false
 
       t.timestamps null: false
     end
