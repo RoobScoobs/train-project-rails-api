3216e5922d4be9cc4d301903e6a1c53c43f7bc30
RoobScoobs <iruben229@gmail.com>
2016-06-23 11:29:01 -0400

    Update models
    Attach relationship and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 #
 class User < ActiveRecord::Base
   include Authentication
-  has_many :examples
+  has_many :workouts
+  validates :email, uniqueness: true
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,4 @@
 class Workout < ActiveRecord::Base
   belongs_to :user
+  validates :user, presence: true
 end
