b1b7ab7b9cd41c7b4c05ae3862d321a71918127b
RoobScoobs <iruben229@gmail.com>
2016-06-24 12:20:26 -0400

    Update migrations
    Replace workouts w/ runs

--- db/migrate/20160624160218_create_runs.rb
+++ db/migrate/20160624160218_create_runs.rb
@@ -1,10 +1,11 @@
 class CreateRuns < ActiveRecord::Migration
   def change
     create_table :runs do |t|
-      t.text :description
-      t.decimal :total_miles
-      t.time :total_time
-      t.date :completion_date
+      t.references :user, index: true, foreign_key: true, null: false
+      t.text :description, null: false
+      t.decimal :total_miles, null: false
+      t.time :total_time, null: false
+      t.date :completion_date, null: false
 
       t.timestamps null: false
     end
